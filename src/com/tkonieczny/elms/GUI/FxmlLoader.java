ae670d7d29e232bfcebf915d75701720a573f70f
tkonieczny <konieczny.t@icloud.com>
2020-06-03 19:10:31 +0200

    Scenes (GUI) v2

---
+++ src/com/tkonieczny/elms/GUI/FxmlLoader.java
@@ -0,0 +1,25 @@
+package com.tkonieczny.elms.GUI;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+
+public class FxmlLoader {
+    private Pane view;
+
+    public Pane getPage(String fileName) {
+        try {
+            URL fileUrl = MainScene.class.getResource("/GUI/" + fileName + ".fxml");
+            if(fileUrl==null){
+                throw new java.io.FileNotFoundException(" This .FXML file isn't possible to be found");
+            }
+            view = new FXMLLoader().load(fileUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return view;
+    }
+}
--- src/com/tkonieczny/elms/GUI/Main.java
+++ src/com/tkonieczny/elms/GUI/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1240, 780));
         primaryStage.show();
     }
 
